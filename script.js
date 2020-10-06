71d69eabd00bef213d810e9da49adf5b49287c62
kadli <honza.pavouk@seznam.cz>
2020-10-06 13:43:56 +0000

    neco idk

--- script.js
+++ script.js
@@ -11,8 +11,31 @@
 function vysledek() {
    let c1 = document.getElementById("cislo1").valueAsNumber;
    let c2 = document.getElementById("cislo2").valueAsNumber;
-   let vysl = c1 + c2;
+   let op = document.getElementById("operace").value;
+   console.log(op);
+
+
+
+
+
+   let vysl = "?";
+   if (isNaN(c1)) {
+     alert("Neni zadano prvni cislo");
+     }
+     
+     else if (isNaN(c2)) {
+       alert ("Neni zadano druhe cislo");
+      
+     }
+    
+   else if (op == "plus") {
+       vysl = c1 + c2; 
+  }
+     else if (op == "minus") {
+       vysl = c1 - c2;
+  }
    //výpis do konzole 
    console.log(vysl);
-   
+   document.getElementById("vypocet").value= vysl;
+   document.getElementById("vysledek2").innerHTML = "Výsledek je <b> " + vysl + "</b>";
    }
