6100ee2c91f30e795a381f96257430fca50bda41
kadli <honza.pavouk@seznam.cz>
2020-10-06 13:13:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+ function zdravickohellou() {
+   let jm = document.getElementById("jmeno").value;
+   //výpis do konzole 
+   console.log(jm);
+   //alert("zdravim " + jm + "!");
+}
+
+
+
+
+function vysledek() {
+   let c1 = document.getElementById("cislo1").valueAsNumber;
+   let c2 = document.getElementById("cislo2").valueAsNumber;
+   let vysl = c1 + c2;
+   //výpis do konzole 
+   console.log(vysl);
+   
+   }
